10bf7eb443bd0b3b259fc267d1a5c5e80d00fe02
Sachin <sachin@solliance.net>
2018-05-07 18:22:49 +0530

    ARM: extension name, sh: install hey fix

--- aerospike-server-latency/setup-hey-client.sh
+++ aerospike-server-latency/setup-hey-client.sh
@@ -10,17 +10,18 @@ wget https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.10.2.linux-amd64.tar.gz
 echo export PATH=$PATH:/usr/local/go/bin >> /etc/profile
 source /etc/profile
-go version >> /root/setup-logs/hey-installation.log
+go version 2>> /root/setup-logs/hey-installation.log >> /root/setup-logs/hey-installation.log
 
 # install hey
 printf "\nInstalling hey client...\n"  >> /root/setup-logs/hey-installation.log
-go get -u -v github.com/rakyll/hey 2>>/root/setup-logs/hey-installation.log
+/usr/local/go/bin/go get -u -v github.com/rakyll/hey 2>> /root/setup-logs/hey-installation.log
 ls -al /root/go/bin >> /root/setup-logs/hey-installation.log
 
 # setup ulimit 
 printf "\nSetting up ulimit...\n"  >> /root/setup-logs/hey-installation.log
 echo ulimit -n 635535 >> /etc/profile
 source /etc/profile
+ulimit -n 2>> /root/setup-logs/hey-installation.log >> /root/setup-logs/hey-installation.log
 ulimit -a >> /root/setup-logs/hey-installation.log
 
 # setup sysctl
