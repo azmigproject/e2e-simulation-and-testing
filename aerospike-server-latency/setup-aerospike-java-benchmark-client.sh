cb1319e8e2d3bdb7f5fe1d272f9e4a4fbc967660
Sachin <sachin@solliance.net>
2018-05-07 16:05:42 +0530

    Add ulimit and sysctl configuration.

--- aerospike-server-latency/setup-aerospike-java-benchmark-client.sh
+++ aerospike-server-latency/setup-aerospike-java-benchmark-client.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-apt-get update -y
-apt-get install -y openjdk-8*
-apt-get install -y maven maven*
-git clone https://github.com/aerospike/aerospike-client-java.git /root/aerospike-client-java
+
+apt-get update
+mkdir setup-logs
+
+# install and setup Java client
+apt-get install -y openjdk-8* >> setup-logs/java-client-installation.log
+apt-get install -y maven maven* >> setup-logs/java-client-installation.log
+git clone https://github.com/aerospike/aerospike-client-java.git /root/aerospike-client-java >> setup-logs/java-client-installation.log
 cd /root/aerospike-client-java
 cp pom.xml pom.xml.bak
 dependency="<dependencies>\n<dependency>\n<groupId>com.aerospike</groupId>\n<artifactId>aerospike-client</artifactId>\n<version>4.0.6</version>\n</dependency>\n</dependencies>"
 temp=$(echo $dependency | sed 's/\//\\\//g')
 sed "/<\/project>/ s/.*/${temp}\n&/" pom.xml
-./build_all
+more pom.xml >> setup-logs/java-client-installation.log
+./build_all >> setup-logs/java-client-installation.log
+
+# setup ulimit 
+ulimit -n 65535
+
+# setup sysctl
+cat >> /etc/sysctl.conf << EOF
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_tw_reuse = 1
+EOF
+shutdown -r
--- aerospike-server-latency/setup-aerospike-server.sh
+++ aerospike-server-latency/setup-aerospike-server.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# install Aerospike server
 apt-get update
-pip install --upgrade pip
-wget -O aerospike-server.tgz https://www.aerospike.com/download/server/latest/artifact/ubuntu16
+mkdir setup-logs
+
+# install Aerospike server
+pip install --upgrade pip >> setup-logs/aerospike-installation.log
+wget -O aerospike-server.tgz https://www.aerospike.com/download/server/latest/artifact/ubuntu16 >> setup-logs/aerospike-installation.log
 tar -zxvf aerospike-server.tgz
 cd aerospike-server-community-*
-./asinstall
+./asinstall >> setup-logs/aerospike-installation.log
 
 # Install Aerospike Management Console (AMC)
-wget -O aerospike-management-console.deb https://www.aerospike.com/download/amc/latest/artifact/ubuntu12
+wget -O aerospike-management-console.deb https://www.aerospike.com/download/amc/latest/artifact/ubuntu12 >> setup-logs/aerospike-installation.log
 dpkg -i aerospike-management-console.deb
-apt-get -f install
+apt-get -f install >> setup-logs/aerospike-installation.log
 
 # Start Aerospike and AMC at start up
 sed -i.bak '/exit 0/d' /etc/rc.local
@@ -27,5 +29,14 @@ then
     exit $?
 fi
 exit 0
+EOF
+
+# setup ulimit 
+ulimit -n 65535
+
+# setup sysctl
+cat >> /etc/sysctl.conf << EOF
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_tw_reuse = 1
 EOF
 shutdown -r
