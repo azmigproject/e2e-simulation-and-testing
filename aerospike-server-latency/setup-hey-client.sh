e153149574a631367a92683e4d47b429df93ab36
Sachin <sachin@solliance.net>
2018-05-07 19:11:28 +0530

    hey setup fix

--- aerospike-server-latency/setup-hey-client.sh
+++ aerospike-server-latency/setup-hey-client.sh
@@ -17,16 +17,10 @@ then
     printf "\n[Retry] Checking version of Go...\n" >> /root/setup-logs/hey-installation.log
     /usr/local/go/bin/go version 2>> /root/setup-logs/hey-installation.log >> /root/setup-logs/hey-installation.log
 fi
-printf "\nChecking GOPATH...\n" >> /root/setup-logs/hey-installation.log
-gopath=$(/usr/local/go/bin/go env GOPATH)
-if [ "$gopath" == "" ]
-then    
-    printf "\nGOPATH not set.\n" >> /root/setup-logs/hey-installation.log
-    printf "\nSetting GOPATH...\n" >> /root/setup-logs/hey-installation.log
-    echo export GOPATH=$HOME/go >> ~/.profile
-    source ~/.profile
-fi
-echo $gopath >> /root/setup-logs/hey-installation.log
+printf "\nSetting GOPATH...\n" >> /root/setup-logs/hey-installation.log
+echo export GOPATH=$HOME/go >> ~/.profile
+source ~/.profile
+echo GOPATH=$GOPATH >> /root/setup-logs/hey-installation.log
 
 # install hey
 printf "\nInstalling hey client...\n" >> /root/setup-logs/hey-installation.log
@@ -38,8 +32,6 @@ ls -al /root/go/bin >> /root/setup-logs/hey-installation.log
 printf "\nSetting up ulimit...\n" >> /root/setup-logs/hey-installation.log
 echo ulimit -n 635535 >> /etc/profile
 source /etc/profile
-ulimit -n 2>> /root/setup-logs/hey-installation.log >> /root/setup-logs/hey-installation.log
-ulimit -a >> /root/setup-logs/hey-installation.log
 
 # setup sysctl
 printf "\nSetting up sysctl values...\n" >> /root/setup-logs/hey-installation.log
@@ -47,4 +39,6 @@ cat >> /etc/sysctl.conf << EOF
 net.ipv4.tcp_tw_recycle = 1
 net.ipv4.tcp_tw_reuse = 1
 EOF
+
+printf "\nComplete.\nRestarting...\n"
 shutdown -r
