f9af494c700da747415676eeaedae0df21ecde4e
Sachin <sachin@solliance.net>
2018-06-06 16:30:01 +0530

    added haproxy configuration

--- jmeter-tests/haproxy-vm/setup-haproxy.sh
+++ jmeter-tests/haproxy-vm/setup-haproxy.sh
@@ -2,3 +2,18 @@
 
 apt-get update -y
 apt-get install -y haproxy
+cat >> /etc/haproxy/haproxy.cfg << EOF
+
+frontend http_front
+    bind *:80
+    mode http
+    option http-keep-alive
+    default_backend service2_back
+
+backend service2_back
+    mode http
+    balance roundrobin
+    option http-keep-alive
+    server docker-vm-service2v1 172.17.17.6:8882
+    server docker-vm-service2v2 172.17.17.7:8882
+EOF
